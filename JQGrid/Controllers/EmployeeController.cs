667769350a640585fb4a6406de904a5659b7f0ff
maamirfarooq <maamirfarooq87@gmail.com>
2019-04-05 10:34:23 +0500

    JQueryGrid
    JQueryGrid with EntityFrameworks

---
+++ JQGrid/Controllers/EmployeeController.cs
@@ -0,0 +1,26 @@
+﻿using JQGrid.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JQGrid.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult GetData()
+        {
+            using (DBModel db = new DBModel())
+            {
+                var employeeList = db.Employees.ToList<Employee>();
+                return Json(new { rows = employeeList }, JsonRequestBehavior.AllowGet);
+            }
+        }
+    }
+}
